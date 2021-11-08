a10dbb3118a9da70192238b4a24ce5501e7d69b3
Zac Blaz <zacblaz@hotmail.com>
2021-11-09 09:17:45 +1100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿bool keepLooping = true;
+
+    Console.WriteLine("Please enter your name!:");
+
+    string name = Console.ReadLine();
+
+    Console.WriteLine("Oh, so your name is " + name, "?");
+while (true)
+{
+    Console.WriteLine("Y=1/N=2");
+
+    int nameconfirm = int.Parse(Console.ReadLine());
+
+    switch (nameconfirm)
+    {
+      case 1:
+            Console.WriteLine(name, " that's a good name");
+            break;
+
+      case 2:
+            
+
+        default:
+            Console.WriteLine("That isn't a valid input");
+            continue;
+    }
+}
